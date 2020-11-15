e1a4c2b45b3b59c2a9416f51717f600c8b790bb6
Andreas Berg <51373685+BollaBerg@users.noreply.github.com>
2020-11-15 18:45:18 +0100

    Update Coverage.svg-saving to work without existing file

--- generate_coverage.py
+++ generate_coverage.py
@@ -25,4 +25,7 @@ if __name__ == '__main__':
 
     badge = anybadge.Badge("Coverage", value, thresholds=tresholds, value_suffix='%')
 
-    badge.write_badge("docs/img/Coverage.svg", overwrite=True)
+    try:
+        badge.write_badge("docs/img/Coverage.svg")
+    except:
+        badge.write_badge("docs/img/Coverage.svg", overwrite=True)
