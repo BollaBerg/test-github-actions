eef9b8f00a075bc2ffac36dfef815d839a20a3cc
Andreas Berg <51373685+BollaBerg@users.noreply.github.com>
2020-11-15 02:05:08 +0100

    KNOWN FALSE: Update test.py with test of func2

--- test.py
+++ test.py
@@ -5,3 +5,6 @@ from to_be_tested import func1, func2, func3
 class Test(unittest.TestCase):
     def test_func1(self):
         self.assertEqual(func1(), True)
+
+    def test_func2(self):
+        self.assertEqual(func2(10, 20), 40)
